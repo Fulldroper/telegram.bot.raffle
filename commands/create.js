bf817b8473f08b98c3ef1a5fa1729db1ff227fb0
full_droper <full_droper@pm.me>
2022-12-06 06:06:11 +0300

    'upd'

--- commands/create.js
+++ commands/create.js
@@ -409,6 +409,8 @@ module.exports.modal = async function (interaction) {
         }
         embeds[0].footer.text += timeString
       };
+      console.log(users);
+      console.log(users?.declension());
       if (users) embeds[0].footer.text += users.declension();
       const components = []
 
