190d80d9bb7edc5f2f9ff011a8843eaf80fe8b92
full_droper <full_droper@pm.me>
2022-12-06 08:13:31 +0300

    'upd'

--- commands/reset.js
+++ commands/reset.js
@@ -6,12 +6,14 @@ module.exports.info = {
 }
 
 module.exports.run = async function(interaction) {
-  if (interaction.member.permissions.serialize().Administrator) {
+  const id = `${this.user.username}:${interaction.guildId}:allow:${interaction.commandName || interaction.meta[0]}`
+  const allowed = await this.db.get(id) || []
+  if (interaction.member.permissions.serialize().Administrator || allowed.includes(interaction.member.id)) {
     await this.db.clear(`${this.user.username}:${interaction.guildId}:stats`)
   
     interaction.reply({
       content: `Топ 10 сервера ${interaction.guild.name} очищенно`,
       ephemeral: true 
     })
-  } else interaction.reply({ content: '❌ Ви маєте мати права адміністратора', ephemeral: true }).catch(e => console.error(e));
+  } else interaction.reply({ content: '❌ У вас недостатньо прав для виконання команди', ephemeral: true }).catch(e => console.error(e));
 }
